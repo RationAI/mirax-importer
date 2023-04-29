d54ec2b6cbe7eab38787d72428dbb9ca903e992a
Aiosa <horakj7@gmail.com>
2023-04-29 20:01:45 +0200

    Incorrect shell command in the importer job 6.

--- server/kubernetes/importer_job.py
+++ server/kubernetes/importer_job.py
@@ -62,7 +62,7 @@ spec:
         command: ['bash']
         args:
         - -c
-        - mkdir -p /var/www/html/importer && git clone --single-branch --branch kubernetes https://github.com/RationAI/mirax-importer /var/www/html/importer && mkdir -p /var/www/html/xo_db && git clone https://github.com/RationAI/xo_db /var/www/html/xo_db && {command} >> "{logfile}" 2>&1
+        - mkdir -p /var/www/html/importer && git clone --single-branch --branch kubernetes https://github.com/RationAI/mirax-importer /var/www/html/importer && {command} >> "{logfile}" 2>&1
         securityContext:
           runAsUser: 33
           runAsGroup: 33
